0e03b948f864868bd7626d719c531b91f8f5dcbd
Crank <crank@crank.ru>
2018-11-28 21:30:37 +0300

    true package structure

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from livolo import Livolo
--- setup.py
+++ setup.py
@@ -7,18 +7,22 @@ AUTHOR = "Crank"
 AUTHOR_EMAIL = "crank@crank.ru"
 URL = "https://github.com/crankru/pylivolo"
 # VERSION = __import__(PACKAGE).__version__
-VERSION = 0.1
+VERSION = '0.1.1'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
  
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    # long_description=read("README.rst"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="BSD",
     url=URL,
-    packages=find_packages(exclude=["tests.*", "tests", 'blynk.py']),
+    packages=find_packages(exclude=["tests.*", "tests"]),
     # install_requires=[],
     # package_data=find_package_data(
     #     PACKAGE,
@@ -26,12 +30,10 @@ setup(
     # ),
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
+        "Operating System :: Raspbian",
         "Programming Language :: Python",
-        "Framework :: Django",
     ],
     zip_safe=False,
 )
